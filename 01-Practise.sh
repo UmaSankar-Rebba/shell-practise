fe5d5452a879f4fe27a5dc13d0ff4bf6681d2bd8
UmaSankarRebba <umasankarrebba@gmail.com>
2026-03-08 11:42:12 +0530

    new chnages

--- 01-Practise.sh
+++ 01-Practise.sh
@@ -6,5 +6,5 @@ echo "omkar:Hi uma! I am fine.what about you?"
 echo "uma:i am good omkar.actually i am going to chennai.do you wanna join with me?"
 echo "omkar:Hooo...! acn you please specify the date?"
 echo "uma:On 4Th April"
-echo "omkar"Okay uma i am free on the specified date i will join with you"
+echo "omkar:Okay uma i am free on the specified date i will join with you"
 
