7529c65582deebc6f1607be5fc5f5b1f2658c888
UmaSankarRebba <umasankarrebba@gmail.com>
2026-03-08 11:33:28 +0530

    i am adding new file

---
+++ 01-Practise.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+#i am printing some statements
+echo "uma: Hi omkar! how are you?"
+echo "omkar:Hi uma! I am fine.what about you?"
+echo "uma:i am good omkar.actually i am going to chennai.do you wanna join with me?"
+echo "omkar:Hooo...! acn you please specify the date?"
+echo "uma:On 4Th April"
+echo "omkar"Okay uma i am free on the specified date i will join with you"
+
