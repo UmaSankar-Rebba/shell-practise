bf722f5dbc156bd64cc16cefe370c1759a5d15df
UmaSankarRebba <umasankarrebba@gmail.com>
2026-03-08 12:08:38 +0530

    new chnages

---
+++ 02-Variable.sh
@@ -0,0 +1,11 @@
+#!/bash/bin
+#Now i am printing using varaibles
+VAR_NAME1=Uma
+VAR_NAME2=Omkar
+
+echo "$VAR_NAME1: Hi $VAR_NAME2! how are you?"
+echo "$VAR_NAME1:Hi $VAR_NAME1! I am fine.what about you?"
+echo "$VAR_NAME1:i am good $VAR_NAME2.actually i am going to chennai.do you wanna join with me?"
+echo "$VAR_NAME2:Hooo...! acn you please specify the date?"
+echo "$VAR_NAME1:On 4Th April"
+echo "$VAR_NAME2:Okay $VAR_NAME1 i am free on the specified date i will join with you"
