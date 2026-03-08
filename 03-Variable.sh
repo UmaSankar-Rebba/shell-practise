3fc3b00bd71623b530fdffbe84c69d3e4238a8ce
UmaSankarRebba <umasankarrebba@gmail.com>
2026-03-08 12:21:44 +0530

    New Changes Required

--- 03-Variable.sh
+++ 03-Variable.sh
@@ -2,3 +2,7 @@
 echo "please enter the username"
 read USER_NAME
 echo "Your username is $USER_NAME"
+#please enter your password
+echo "please enter your password"
+read -s PASS_WORD
+echo "your password is $PASS_WORD"
