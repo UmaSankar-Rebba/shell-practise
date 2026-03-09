84a560e36bacfde4bb3bb151f87ffe0cde18072c
UmaSankarRebba <umasankarrebba@gmail.com>
2026-03-09 12:20:10 +0530

    New Changes Required

--- 07-conditions.sh
+++ 07-conditions.sh
@@ -1,9 +1,13 @@
 #!/bash/bin
 NUM=25
 #here we are going to write some conditions
+#-gt means greater than
+# -lt means less than
+# -eq means equal to
+#-ne means not equals to
 
 if [ $NUM -gt 9 ]; then
- echo "Given num is TRUE"
+ echo "Given num is Greater than $NUM"
 else
- echo "Given num is FALSE"
- fi
+ echo "Given num is less than $NUM"
+fi
