42375aed12558b73824d2adc04b449723b1e68f9
UmaSankarRebba <umasankarrebba@gmail.com>
2026-03-09 12:37:41 +0530

    New Changes Required

--- 07-conditions.sh
+++ 07-conditions.sh
@@ -1,16 +1,17 @@
 #!/bash/bin
 echo "enter the number"
 read NUM
+NUM2=25
 #here we are going to write some conditions
 #-gt means greater than
 # -lt means less than
 # -eq means equal to
 #-ne means not equals to
 
-if [ $NUM -gt 25 ]; then
- echo "Given num is Greater than $NUM"
-elif [ $NUM -eq 25 ]; then
- echo "Given number is equals to $NUM"
+if [ $NUM -gt $NUM2 ]; then
+ echo "Given num is Greater than $NUM2"
+elif [ $NUM -eq $NUM2 ]; then
+ echo "Given number is equals to $NUM2"
 else
- echo "Given num is less than $NUM"
+ echo "Given num is less than $NUM2"
 fi
