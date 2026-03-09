191c0d13fc55a1493da916c87653d9eccee70615
UmaSankarRebba <umasankarrebba@gmail.com>
2026-03-09 10:57:19 +0530

    New Changes Required

--- 06-data-types.sh
+++ 06-data-types.sh
@@ -8,5 +8,5 @@ echo "the total sum is $SUM"
 Fruits=("apple" "banana" "kiwi")
 echo "i am having ${Fruits[@]}"
 echo "My morning fruit is ${Fruits[1]}"
-echo "My afternoon fruit is ${Fruit[0]}"
-echo "My evening snack fruit is ${Fruit[2]}"
+echo "My afternoon fruit is ${Fruits[0]}"
+echo "My evening snack fruit is ${Fruits[2]}"
