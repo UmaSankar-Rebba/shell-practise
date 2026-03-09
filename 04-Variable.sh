602c0053e3bf0d0adf3369558b42e837a4114aa6
UmaSankarRebba <umasankarrebba@gmail.com>
2026-03-09 10:18:15 +0530

    New Changes Required

--- 04-Variable.sh
+++ 04-Variable.sh
@@ -2,6 +2,7 @@
 TIME_START=$(date +%s)
 echo "the starting time is $TIME_START"
 sleep 10
-TIME_END=$(date)
+TIME_END=$(date +%s)
 echo "the ending time is $TIME_END"
+echo "Total time is $TIME_START-$TIME_END"
 
