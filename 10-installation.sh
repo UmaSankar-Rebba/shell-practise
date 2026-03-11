5d9f343d0f958b9627333d290ee39a8b724d80a5
UmaSankarRebba <umasankarrebba@gmail.com>
2026-03-11 12:24:29 +0530

    New Changes Required

---
+++ 10-installation.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+USERID=$(id -u)
+
+if [$USERID -ne 0 ]; then
+ echo "You dont have permission do to this operation.contact sudo ADMIN"
+else
+ echo "please provide the data to install"
+ read SYSTEM
+ dnf install $SYSTEM -y
+ echo " $SYSTEM Installation is succes"
+fi
+
