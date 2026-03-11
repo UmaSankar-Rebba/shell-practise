f829f427b50a43802230e621d9b263a4f3fcf6d3
UmaSankarRebba <umasankarrebba@gmail.com>
2026-03-11 12:07:57 +0530

    New Changes Required

--- 09-installation.sh
+++ 09-installation.sh
@@ -6,3 +6,14 @@ if [ $USERID -ne 0 ]; then
  echo "You dont have permission to do this task.Contact SUDO admin"
  exit 1
 fi
+ 
+ VALIDATE (){
+    if [ $1 -ne 0 ]; then
+     echo " $2 is failure"
+    else
+     echo "$2 is Success"
+ }
+
+ dnf install nginx -y
+ VALIDATE $? "Installaing nginx"
+
