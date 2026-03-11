ce59ac70e591e0cd5c7731d0c5e438fd1c1d517b
UmaSankarRebba <umasankarrebba@gmail.com>
2026-03-11 12:28:04 +0530

    New Changes Required

--- 10-installation.sh
+++ 10-installation.sh
@@ -4,6 +4,7 @@ USERID=$(id -u)
 
 if [$USERID -ne 0 ]; then
  echo "You dont have permission do to this operation.contact sudo ADMIN"
+ exit 1
 else
  echo "please provide the data to install"
  read SYSTEM
