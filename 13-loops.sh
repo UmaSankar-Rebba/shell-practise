7123d0344e3b0c439a2900b81384a63d729a816a
UmaSankarRebba <umasankarrebba@gmail.com>
2026-03-14 10:50:40 +0530

    New Changes Required

---
+++ 13-loops.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+USERID=$(id -u)
+
+if [ $USERID -ne 0 ]; then
+ echo "You dont have permission to do this task.Contact SUDO admin"
+ exit 1
+fi
+ 
+
+VALIDATE(){
+    if [ $1 -ne 0 ]; then
+     echo " $2 is failure"
+    else
+     echo "$2 is Success"
+    fi
+}
+ for PACKAGE in $@
+ do
+ dnf install $PACKAGE -y
+VALIDATE $0 "$PACKAGE installing"
+ done
+
