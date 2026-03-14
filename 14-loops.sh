fe21143ea678cc05e0cc3e4d09f0f0c750e2b0cb
UmaSankarRebba <umasankarrebba@gmail.com>
2026-03-14 11:10:46 +0530

    New Changes Required

---
+++ 14-loops.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+USERID=$(id -u)
+LOGS_FOLDER="/var/log/shell-script"
+LOGS_FILE="/var/log/shell-script/$0.log"
+
+if [ $USERID -ne 0 ]; then
+ echo "You dont have permission to do this task.Contact SUDO admin"
+ exit 1
+fi
+ 
+
+VALIDATE(){
+    if [ $1 -ne 0 ]; then
+     echo " $2 is failure"
+    else
+     echo "$2 is Success"
+    fi
+}
+ for PACKAGE in $@
+ do
+ dnf list installed $PACKAGE
+ EXIT=($?)
+ if [ $EXIT = 0]; then
+  echo "$PACKAGE Is already INSTALLED" | tee -a $LOGS_FILE
+ else
+  dnf install $PACKAGE -y | tee -a $LOGS_FILE
+  VALIDATE $0 "$PACKAGE installing"
+ done
+
