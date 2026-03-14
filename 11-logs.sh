089548a9389099e2c5942a1df9accdcdc2d45157
UmaSankarRebba <umasankarrebba@gmail.com>
2026-03-14 10:25:04 +0530

    New Changes Required

--- 11-logs.sh
+++ 11-logs.sh
@@ -7,7 +7,7 @@ LOGS_FILE="/var/log/shell-script/$0.log"
 mkdir -p $LOGS_FOLDER
 
 if [ $USERID -ne 0 ]; then
- echo "You dont have permission do to this operation.contact sudo ADMIN"
+ echo "You dont have permission do to this operation.contact sudo ADMIN" | tee -a $LOGS_FILE
  exit 1
 else
  echo "please provide the data to install and"
